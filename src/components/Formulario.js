58c9c085eefa6813184bfe070ef2c31fec5df09c
Martin <martin_leiva13@hotmail.com>
2020-01-23 20:15:51 -0300

    Componentes usados en el proyecto

---
+++ src/components/Formulario.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const Formulario = ({datosConsulta}) => {
+
+    //State del componente
+    //busqueda = state, guardarBusqueda = this.setState({})
+    const [busqueda, guardarBusqueda] = useState({
+        ciudad: '',
+        pais: '',
+    })
+
+    const handleChange = (e) => {
+        //Cambiar el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const consultarClima = (e) => {
+        e.preventDefault();
+
+        //Pasar al componente principal la busqueda del usuario
+        datosConsulta(busqueda);
+    }
+
+    return (
+        <form onSubmit={consultarClima}>
+            <div className="input-field col s12">
+                <input 
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    onChange={handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <select name="pais" onChange={handleChange}>
+                    <option value="">Selecciona un país</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+            </div>
+
+            <div className="input-field col s12">
+                <input type="submit" value="Buscar clima" className="waves-effect waves-light btn-large btn-block yellow accent-4" />
+            </div>
+
+        </form>
+    );
+};
+
+export default Formulario;
